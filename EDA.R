a3ef10b60e79f0bed3df4d6c19f1b54340478933
Aditya Khanna <khanna7.work@gmail.com>
2022-02-28 21:04:13 -0500

    lines to access data using SMB

--- EDA.R
+++ EDA.R
@@ -8,15 +8,17 @@ library(dplyr)
 
 # Set wd for generated output ---------------------------
 
-setwd("~/code/rtp-network/out")
-
+setwd("./out")
+#load("EDA.RData") #When mounting using SMB, uncomment
 
 # Read data ---------------------------
 
 data_dir <- "/gpfs/data/rkantor/rtp/datasets/D30_20211013_V1"
 list.files(path=data_dir)
-          
 net_dt <- read.csv(paste0(data_dir, "/ContactTracingNetwork.csv"))
+
+## when mounting using SMB, comment above three lines
+          
 dim(net_dt)
 str(net_dt)
 View(net_dt)
