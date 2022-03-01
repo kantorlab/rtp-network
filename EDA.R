23ee528b1af11a1d0f11394f5d726c9efba9d1cc
Aditya Khanna <akhann16@node1151.oscar.ccv.brown.edu>
2022-02-28 20:59:49 -0500

    analyze reachability stats

--- EDA.R
+++ EDA.R
@@ -1,12 +1,55 @@
-# read data
+rm(list=ls())
 
-setwd("/gpfs/data/rkantor/rtp/datasets/D30_20211013_V1")
-ls()
-list.files()
 
-net <- read.csv("ContactTracingNetwork.csv")
-dim(net)
-str(net)
-View(net)
+# Load libraries ---------------------------
 
-table(net$InterviewRank, exclude = NULL)
+library(dplyr)
+
+
+# Set wd for generated output ---------------------------
+
+setwd("~/code/rtp-network/out")
+
+
+# Read data ---------------------------
+
+data_dir <- "/gpfs/data/rkantor/rtp/datasets/D30_20211013_V1"
+list.files(path=data_dir)
+          
+net_dt <- read.csv(paste0(data_dir, "/ContactTracingNetwork.csv"))
+dim(net_dt)
+str(net_dt)
+View(net_dt)
+
+table(net_dt$InterviewRank, exclude = NULL)
+
+
+# Filter by rank 1 ---------------------------
+
+net_dt_r1 <- net_dt %>% filter(InterviewRank == 1)
+
+dim(net_dt_r1)
+str(net_dt_r1)
+View(net_dt_r1)
+
+table(net_dt_r1$InterviewRank, exclude = NULL)
+table(net_dt_r1$InterviewRank, exclude = NULL)/sum(table(net_dt_r1$InterviewRank, 
+                                                         exclude = NULL))
+
+table(net_dt_r1$ClientNotReached, exclude = NULL)
+table(net_dt_r1$ClientNotReached, exclude = NULL)/sum(
+  table(net_dt_r1$ClientNotReached, exclude = NULL)
+)
+
+table(net_dt_r1$ClientReached, exclude = NULL)
+table(net_dt_r1$ClientReached, exclude = NULL)/sum(table(net_dt_r1$ClientReached, 
+                                                         exclude = NULL))
+
+
+table(net_dt_r1$AlreadyIndexCase, exclude = NULL)
+table(net_dt_r1$AlreadyIndexCase, exclude = NULL)/sum(table(net_dt_r1$AlreadyIndexCase, exclude = NULL))
+
+
+#Save Object ---------------------------
+
+save.image(file="EDA.RData")
