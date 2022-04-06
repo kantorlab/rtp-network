7c8ae881025b1a39df6a7208f0c6b610fe12cb51
Aditya Khanna <khanna7.work@gmail.com>
2022-04-06 12:15:33 -0400

    add instructions on reading/loading data on remote desktop vs smb. load individual data

--- EDA.R
+++ EDA.R
@@ -7,21 +7,25 @@ library(dplyr)
 library(network)
 
 
-# Set wd for generated output ---------------------------
+# Setup ---------------------------
 
-setwd("./out")
-
-#-------------------
+setwd("/Volumes/home/akhann16/code/rtp-network/out") #on smb
+#setwd("/gpfs/home/akhann16/code/rtp-network/out") #on remote desktop 
 load("EDA.RData") #When mounting using SMB, uncomment
-#-------------------
+par(mar=c(1,1,1,1)) #for figures
+ls()
 
+### 
 
 # Read data ---------------------------
 
+## Comment below lines on SMB
+## Uncomment if needed on remote desktop
 
-#data_dir <- "/gpfs/data/rkantor/rtp/datasets/D30_20211013_V1"
-#list.files(path=data_dir)
-#net_dt <- read.csv(paste0(data_dir, "/ContactTracingNetwork.csv"))
+# data_dir <- "/gpfs/data/rkantor/rtp/datasets/D30_20211013_V1"
+# list.files(path=data_dir)
+# net_dt <- read.csv(paste0(data_dir, "/ContactTracingNetwork.csv"))
+# individuals_dt <- read.csv(paste0(data_dir, "/Individuals.csv"))
 
 #-------------------
 ## when mounting using SMB, comment above three lines
@@ -31,6 +35,9 @@ dim(net_dt)
 str(net_dt)
 sort(colnames(net_dt))
 View(net_dt)
+
+dim(individuals_dt)
+colnames(individuals_dt)
 
 table(net_dt$InterviewRank, exclude = NULL)
 table(net_dt$InterviewRank, exclude = NULL)/
@@ -85,6 +92,7 @@ table(net_dt_r1$InterviewType, exclude = NULL)
 table(net_dt_r1$InterviewType, exclude = NULL)/sum(table(net_dt_r1$InterviewType, exclude = NULL))
 
 #Create network object ---------------------------
+
 net_dt_mat <- as.matrix.data.frame(cbind(net_dt[,1], net_dt[,2]))
 net <- as.network(net_dt_mat, bipartite = F, directed = T)
 net
@@ -94,10 +102,13 @@ net_dt_r1_mat <- as.matrix.data.frame(cbind(net_dt_r1[,1], net_dt_r1[,2]))
 net_r1 <- as.network(net_dt_r1_mat, bipartite = F, directed = T)
 net_r1
 length(net_r1 %v% "vertex.names")
+head(net_r1 %v% "vertex.names")
 
 plot(net)
 plot(net_r1)
 
+
+#Examine network datasets ---------------------------
 
 length(which(net_dt_mat[,1] %in% net_dt_mat[,2]))
 length(which(net_dt_mat[,2] %in% net_dt_mat[,1]))
@@ -108,10 +119,8 @@ length(which(net_dt_r1_mat[,2] %in% net_dt_r1_mat[,1]))
 length(intersect(net_dt_r1_mat[,1], net_dt_r1_mat[,2]))
 
 length(which(unique(net_dt_mat[,1]) %in% (unique(net %v% "vertex.names"))))
-length(which(unique(net_dt_r2_mat[,1]) %in% (unique(net_r2 %v% "vertex.names"))))
+#length(which(unique(net_dt_r2_mat[,1]) %in% (unique(net_r2 %v% "vertex.names"))))
 
-length(which(unique(net_dt_r1_mat[,2]) %in% (unique(net_r2 %v% "vertex.names"))))
-length(which(unique(net_dt_r1_mat[,2]) %in% (unique(net_r1 %v% "vertex.names"))))
 
 #Save Object ---------------------------
 
