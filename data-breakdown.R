2d1a3ae80603133a219c9fc55c39bc2f21eed809
Aditya Khanna <aditya_khanna@brown.edu>
2023-11-03 10:38:48 -0400

    updated version for id week presentation

--- data-breakdown.R
+++ data-breakdown.R
@@ -10,46 +10,61 @@ partner_db_old <- as.data.table(read.csv(paste0(data_dir, "/ContactTracingNetwor
 partner_db <- as.data.table(read.csv("/gpfs/data/rkantor/rtp/shared_dir/ContactTracingNetwork20230726.csv"))
 genomic_db <- as.data.table(read.csv(paste0(data_dir, "/Individuals.csv")))
 
-# compute intersections betwee genomic db and partnerdb
-length(intersect(genomic_db$StudyID, partner_db$StudyIDFrom))
-length(intersect(genomic_db$StudyID, partner_db$StudyIDTo))
+# How many persons in the Genomic DB are sequenced?
+table(genomic_db$Sequence, exclude=NULL)
+genomic_db_sequenced_id <- which(genomic_db$Sequence == "True")
+length(genomic_db_sequenced_id)
+genomic_db_sequenced <- genomic_db$StudyID[genomic_db_sequenced_id]
+genomic_db_sequenced_dt <- genomic_db[genomic_db_sequenced_id,]
+dim(genomic_db_sequenced_dt)
+
+# Compute intersections between sequenced persons in genomic db and partnerdb
+length(intersect(genomic_db_sequenced, partner_db$StudyIDFrom))
+length(intersect(genomic_db_sequenced, partner_db$StudyIDTo))
 length(intersect(partner_db$StudyIDFrom, partner_db$StudyIDTo))
-length(Reduce(intersect, list(genomic_db$StudyID, partner_db$StudyIDFrom, partner_db$StudyIDTo)))
+length(Reduce(intersect, list(genomic_db_sequenced, partner_db$StudyIDFrom, partner_db$StudyIDTo)))
 
 # How many of the interviewed index cases who are also in the genomic DB provided identifiable partner data?
 partner_db_non_missing_studyidto <- partner_db[which(partner_db$StudyIDTo != ""), ]
-index_cases_who_named_partners <- intersect(genomic_db$StudyID, partner_db_non_missing_studyidto$StudyIDFrom)
+index_cases_who_named_partners <- intersect(genomic_db_sequenced, partner_db_non_missing_studyidto$StudyIDFrom)
 length(index_cases_who_named_partners)
 
 # How many of the interviewed index cases who are also in the genomic DB did not provide identifiable partner data?
-intersected_persons_partner_genomic_db <- intersect(genomic_db$StudyID, partner_db$StudyIDFrom)
+intersected_persons_partner_genomic_db <- intersect(genomic_db_sequenced, partner_db$StudyIDFrom)
 intersected_persons_without_named_partner <- intersected_persons_partner_genomic_db[!intersected_persons_partner_genomic_db %in% partner_db_non_missing_studyidto$StudyIDFrom]
 length(intersected_persons_without_named_partner)  
 
-# How many total partners are named by the 497 index cases in the genomic DB who named partners?
+# How many total partners are named by the index cases in the genomic DB who named partners?
 length(unique(partner_db_non_missing_studyidto$StudyIDTo))
 
 # Compute Jaccard coefficient
 
 ## identify unique individuals
-genomic_individuals <- unique(genomic_db$StudyID)
 partner_individuals_from <- unique(partner_db$StudyIDFrom)
 partner_individuals_to <- unique(partner_db$StudyIDTo)
 
-# combine the 'StudyIDFrom' and 'StudyIDTo' for partner_db
+## combine the 'StudyIDFrom' and 'StudyIDTo' for partner_db
 partner_individuals <- unique(c(partner_individuals_from, partner_individuals_to))
+length(partner_individuals)
 
-# compute intersection and union lengths
-intersection_length <- length(intersect(genomic_individuals, partner_individuals))
-union_length <- length(union(genomic_individuals, partner_individuals))
+## compute intersection and union lengths
+intersection_length <- length(intersect(genomic_db_sequenced, partner_individuals))
+union_length <- length(union(genomic_db_sequenced, partner_individuals))
 
-# Calculate Jaccard Coefficient
+## calculate coefficient
 jaccard_coefficient <- intersection_length / union_length
 jaccard_coefficient
 
+# How many are Genomic DB persons are present/not present in Partner DB and vice versa 
+length(which(genomic_db_sequenced %in% partner_individuals))
+length(which(!genomic_db_sequenced %in% partner_individuals))
+
+length(which(partner_individuals %in% genomic_db_sequenced))
+length(which(!partner_individuals %in% genomic_db_sequenced))
+
 # How many named partners are also sequenced 
-named_partners_in_genomic_db <- intersect(genomic_db$StudyID, partner_db$StudyIDTo)
-id_named_partners_in_genomic_db <- which(genomic_db$StudyID %in% named_partners_in_genomic_db)
+named_partners_in_genomic_db <- intersect(genomic_db_sequenced, partner_db$StudyIDTo)
+id_named_partners_in_genomic_db <- which(genomic_db_sequenced %in% named_partners_in_genomic_db)
 
 length(which(is.na(genomic_db$ClusteredPhyloAny)))
 length(which(is.na(genomic_db$ClusteredPhyloAny)))
@@ -68,3 +83,20 @@ table(partner_db$ClientReached, exclude=NULL)
 table(partner_db$NoReachWhy, exclude=NULL)
 
 table(partner_db[id_unique_named_partners,]$ClientReached, exclude=NULL)
+
+# How many sequenced persons cluster phylogenetically?
+seq_pers_cluster_phylo <- nrow(genomic_db_sequenced_dt) - length(which(is.na(genomic_db_sequenced_dt$ClusteredPhyloAny)))
+seq_pers_cluster_phylo/nrow(genomic_db_sequenced_dt)
+
+# How many index cases (who appear in partner DB) and are sequenced are found to cluster phylogenetically?
+seq_index_cases_nmd_pts <- which(genomic_db_sequenced_dt$StudyID %in% index_cases_who_named_partners)
+seq_index_cases_nmd_pts_dt <- genomic_db_sequenced_dt[seq_index_cases_nmd_pts,]
+nrow(seq_index_cases_nmd_pts_dt) -  length(which(is.na(seq_index_cases_nmd_pts_dt$ClusteredPhyloAny)))
+.Last.value/nrow(seq_index_cases_nmd_pts_dt)
+
+# How many named partners (who appear in partner DB) and are sequenced are found to cluster phylogenetically?
+sequenced_who_are_named_pts <- (intersect(genomic_db_sequenced, partner_db$StudyIDTo))
+sequenced_who_are_named_pts_id <- which(genomic_db_sequenced_dt$StudyID %in% sequenced_who_are_named_pts)
+sequenced_who_are_named_pts_dt <- genomic_db_sequenced_dt[sequenced_who_are_named_pts_id,] 
+nrow(sequenced_who_are_named_pts_dt) -  length(which(is.na(sequenced_who_are_named_pts_dt$ClusteredPhyloAny)))
+.Last.value/nrow(sequenced_who_are_named_pts_dt)
