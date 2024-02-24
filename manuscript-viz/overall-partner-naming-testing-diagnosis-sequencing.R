b6aec17e3fcd7d02ea93aaf9dcd0a9e057976e7b
Aditya Khanna <aditya_khanna@brown.edu>
2024-02-24 13:16:30 -0500

    plot overall cascade for manuscript

---
+++ manuscript-viz/overall-partner-naming-testing-diagnosis-sequencing.R
@@ -0,0 +1,66 @@
+rm(list=ls())
+
+# Loading necessary libraries
+library(tidyr)
+library(ggplot2)
+library(RColorBrewer)
+library(here)
+
+# Structuring the data with the average values
+overall_data <- data.frame(
+  Category = c("Named_Mean", "Contact_Attempted_Mean", "Reached_Mean", "Tested_Mean", "Diagnosed_Mean", "Sequenced_Mean"),
+  Value = c(1342, 880, 873, 338, 152, 152)
+)
+
+# Adjusting the factor levels and labels for the Category column
+overall_data$Category <- factor(overall_data$Category, 
+                                levels = c("Named_Mean", 
+                                           "Contact_Attempted_Mean",
+                                           "Reached_Mean",
+                                           "Tested_Mean",
+                                           "Diagnosed_Mean",
+                                           "Sequenced_Mean"),
+                                labels = c("Named", 
+                                           "Attempted",
+                                           "Reached",
+                                           "Tested", 
+                                           "Diagnosed", 
+                                           "Sequenced"))
+
+# Color settings
+set1_colors <- brewer.pal(6, "Set1")
+levels_to_colors <- c("Named", "Attempted", "Reached", "Tested", "Diagnosed", "Sequenced")
+my_colors <- setNames(set1_colors, levels_to_colors)
+
+
+# Create the labels
+labels_n = c("1342", "880 (66%)", "873(99%)", 
+            "338 (38%)", "152 (45%)", "152 (100%)")
+
+# Updated plot code
+p_overall <- ggplot(overall_data, aes(x = Category, y = Value, fill = Category)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_text(aes(label=labels_n), vjust=-0.5, 
+            #size=6, 
+            fontface="bold") +
+  labs(title = "",
+       y = "Total Partners",
+       x = "") +
+  scale_fill_manual(values=my_colors) +  # Setting colors manually
+  scale_y_continuous(limits = c(0, 1500)) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 12, face = "bold"),
+    axis.title.x = element_text(size = 12, face = "bold", color = "black"),
+    axis.title.y = element_text(size = 12, face = "bold", color = "black"),
+    axis.text.y = element_text(size = 12, face = "bold", color = "black"),
+    axis.text.x = element_text(size = 12, face = "bold", color = "black"),
+    legend.position = "none"   # This will remove the legend
+  )
+
+print(p_overall)
+
+# Save plot
+loc_to_save <- here("manuscript-viz", "overall_cascade.pdf")
+ggsave(loc_to_save, plot = p_overall, 
+      width = 8, height = 6, dpi = 300)
