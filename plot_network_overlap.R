e71d153402c97f5a4cbaa4faa554c8feae57abe3
Aditya Khanna <aditya_khanna@brown.edu>
2024-07-17 21:08:56 -0400

    plot overlapping nodes and links

--- plot_network_overlap.R
+++ plot_network_overlap.R
@@ -1,104 +1,48 @@
 rm(list=ls())
 
-
-# Load libraries ---------------------------
-
+# Load libraries
 library(here)
 library(dplyr)
 library(network)
 library(sna)
 
-
-# Setup ---------------------------
-
+# Setup
 par(mar=c(1,1,1,1)) #for figures
-ls()
-
-### 
-
-
-# Set seed ---------------------------
-
 set.seed(1234567)
 
-
-# Load Objects ---------------------------
-
-## rds object
+# Load Objects
 eda_env <- readRDS("eda_objects.rds")
-names(eda_env)
-
-## networks
 ct_net_unique <- eda_env$ct_net_unique
 phylo_net <- eda_env$phylo_net
-
-## edge lists
 ct_el <- eda_env$ct_el 
 ct_el_unique <- eda_env$ct_el_unique 
 phylo_el <- eda_env$phylo_el 
-
-## common edge lists
 common_links <- eda_env$common_links
 
-## query objects
-ct_net_unique
-phylo_net
-
-head(ct_el_unique)
-head(phylo_el)
-
-
-# Plot the two networks ---------------------------
-
-#gplot(ct_net_unique, usearrows = TRUE)
-#gplot(phylo_net, usearrows = FALSE)
-
-
-# Compute overlap between nodes present in both networks ---------------------------
-
+# Compute overlap between nodes present in both networks
 ct_net_ids <- ct_net_unique %v% "vertex.names"
 phylo_net_ids <- phylo_net %v% "vertex.names"
-
 common_node_ids <- intersect(ct_net_ids, phylo_net_ids)
-length(common_node_ids)
-
 unique_ct_net_ids <- setdiff(ct_net_ids, phylo_net_ids)
-length(unique_ct_net_ids)
-
 unique_phylo_net_ids <- setdiff(phylo_net_ids, ct_net_ids)
-length(unique_phylo_net_ids)
 
-
-# Load overlap between nodes present in both networks ---------------------------
-
+# Create edge lists with names
 el_ct_net_unique <- as.data.frame(as.edgelist(ct_net_unique))
 el_ct_net_unique_named <- apply(el_ct_net_unique, 2, function(x) ct_net_ids[x])
-
 el_phylo_net <- as.data.frame(as.edgelist(phylo_net))
 el_phylo_net_named <- apply(el_phylo_net, 2, function(x) phylo_net_ids[x])
 
-head(el_ct_net_unique_named)
-head(el_phylo_net_named)
-common_links
-
-## common_links' represents edges present in both networks as "node1-node2"
-## convert 'common_links' into a data frame similar to 'ct_el' and 'phylo_el'
-
+# Convert common links to data frame
 common_edges_df <- data.frame(
   V1 = sapply(strsplit(common_links, "-"), `[`, 1),
   V2 = sapply(strsplit(common_links, "-"), `[`, 2)
 )
 
+# Add a common edge indicator to edge lists
+ct_el_unique$common <- with(ct_el_unique, paste(V1, V2, sep = "-") %in% common_links)
+phylo_el$common <- with(phylo_el, paste(V1, V2, sep = "-") %in% common_links)
 
-# Add a common edge indicator to your edge lists
-ct_el_unique$common = with(ct_el_unique, paste(V1, V2, sep = "-") %in% common_links)
-phylo_el$common = with(phylo_el, paste(V1, V2, sep = "-") %in% common_links)
-
-# Plot contact tracing network with common elements -------------------------------------------------------------------------
-
-ct_net_unique
-
-# function to assign colors based on whether an edge is common or not
+# Function to assign colors based on common edges
 get_edge_colors <- function(network, common_edges) {
   apply(network, 1, function(edge) {
     if (paste(edge[1], edge[2], sep = "-") %in% common_edges || paste(edge[2], edge[1], sep = "-") %in% common_edges) {
@@ -109,64 +53,94 @@ get_edge_colors <- function(network, common_edges) {
   })
 }
 
-# get the colors for the edges
+# Get colors for the edges
 ct_edge_colors <- get_edge_colors(ct_el_unique, common_links)
 phylo_edge_colors <- get_edge_colors(phylo_el, common_links)
 
-# Get the colors for the nodes
-ct_node_colors <- ifelse(ct_net_ids %in% common_node_ids, "green", "red")
-phylo_node_colors <- ifelse(phylo_net_ids %in% common_node_ids, "green", "red")
+# Get colors for the nodes
+ct_node_colors <- ifelse(ct_net_ids %in% common_node_ids, "limegreen", "orange")
+phylo_node_colors <- ifelse(phylo_net_ids %in% common_node_ids, "limegreen", "orange")
 
-table(ct_node_colors, exclude = NULL)
-table(phylo_node_colors, exclude = NULL)
+# Node size
+node_size <- 2  # Adjust as necessary
 
-## increase node size
-node_size <- 1.5  # Adjust as necessary
+# Plot contact tracing network
+png("ct_net_plot.png", width=800, height=800)
+gplot(ct_net_unique, gmode="graph", displaylabels = FALSE, 
+      vertex.col=ct_node_colors, edge.col=ct_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+dev.off()
 
-## define bright colors for nodes
-bright_node_colors <- ifelse(ct_net_ids %in% common_node_ids, "limegreen", "orange")
+# Plot phylogenetic network
+png("phylo_net_plot.png", width=800, height=800)
+gplot(phylo_net, gmode="graph", displaylabels = FALSE, 
+      vertex.col=phylo_node_colors, edge.col=phylo_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+dev.off()
 
-## plot contact tracing network 
-set.seed(1234567)
-p1 <- 
-  gplot(ct_net_unique, gmode="graph", displaylabels = FALSE, 
-      vertex.col=bright_node_colors, edge.col=ct_edge_colors, 
-      vertex.cex=node_size, edge.lwd=4)
-
-
-## plot phylo network 
-phylo_net
-bright_node_colors_phylo <- ifelse(phylo_net_ids %in% common_node_ids, "limegreen", "orange")
-
-set.seed(1234567)
-p2 <- 
-  gplot(phylo_net, 
-      gmode="graph", 
-      displaylabels = FALSE, 
-      vertex.col=bright_node_colors_phylo, 
-      edge.col=phylo_edge_colors, 
-      vertex.cex=node_size, 
-      edge.lwd=4)
-
-## side by side
-
+# Plot side by side
 png("side_by_side_plots.png", width=1600, height=800)
 layout(matrix(c(1,2), 1, 2, byrow = TRUE))
-
-set.seed(1234567)
 gplot(ct_net_unique, gmode="graph", displaylabels = FALSE, 
-      vertex.col=bright_node_colors, edge.col=ct_edge_colors, 
-      vertex.cex=node_size, edge.lwd=4)
-
-set.seed(1234567)
-gplot(phylo_net, 
-      gmode="graph", 
-      displaylabels = FALSE, 
-      vertex.col=bright_node_colors_phylo, 
-      edge.col=phylo_edge_colors, 
-      vertex.cex=node_size, 
-      edge.lwd=4)
-
+      vertex.col=ct_node_colors, edge.col=ct_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+gplot(phylo_net, gmode="graph", displaylabels = FALSE, 
+      vertex.col=phylo_node_colors, edge.col=phylo_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
 dev.off()
 
 
+# Plot just the overlapping nodes and edges ---------
+
+# Extract the overlapping nodes
+common_node_ids <- intersect(ct_net_ids, phylo_net_ids)
+
+# Extract the overlapping edges
+common_edges_df <- data.frame(
+  V1 = sapply(strsplit(common_links, "-"), `[`, 1),
+  V2 = sapply(strsplit(common_links, "-"), `[`, 2)
+)
+
+# Create sub-networks with only overlapping nodes and edges
+ct_net_overlap <- network(ct_el_unique[ct_el_unique$common == TRUE, 1:2], directed = FALSE)
+phylo_net_overlap <- network(phylo_el[phylo_el$common == TRUE, 1:2], directed = FALSE)
+
+# Set vertex names for overlapping networks
+network.vertex.names(ct_net_overlap) <- common_node_ids
+network.vertex.names(phylo_net_overlap) <- common_node_ids
+
+# Set colors for nodes and edges
+overlap_node_colors <- "limegreen"
+overlap_edge_colors <- "green"
+node_size <- 2  # Adjust as necessary
+
+# Plot overlapping contact tracing network
+png("ct_net_overlap_plot.png", width=800, height=800)
+gplot(ct_net_overlap, gmode="graph", displaylabels = FALSE, 
+      vertex.col=overlap_node_colors, edge.col=overlap_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+dev.off()
+
+# Plot overlapping phylogenetic network
+png("phylo_net_overlap_plot.png", width=800, height=800)
+gplot(phylo_net_overlap, gmode="graph", displaylabels = FALSE, 
+      vertex.col=overlap_node_colors, edge.col=overlap_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+dev.off()
+
+# Plot side by side overlapping networks
+png("side_by_side_overlap_plots.png", width=1600, height=800)
+layout(matrix(c(1,2), 1, 2, byrow = TRUE))
+gplot(ct_net_overlap, gmode="graph", displaylabels = FALSE, 
+      vertex.col=overlap_node_colors, edge.col=overlap_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+gplot(phylo_net_overlap, gmode="graph", displaylabels = FALSE, 
+      vertex.col=overlap_node_colors, edge.col=overlap_edge_colors, 
+      vertex.cex=node_size, edge.lwd=3)
+dev.off()
+
+
+
+# Try making the node placement identical ---------
+
+## layout coords will need to be specified. (try later)
